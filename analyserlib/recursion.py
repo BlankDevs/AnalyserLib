49a60b9e904371885c64d46237880c2524c60f77
Wisani Baloyi <wisanibaloyi65@gmail.com>
2019-03-19 14:25:19 +0200

    Python recusrion and sorting library commit

--- analyserlib/recursion.py
+++ analyserlib/recursion.py
@@ -47,16 +47,11 @@ def fibonacci(n):
         2
     '''
     
-    fibonacci_err = 'Please enter a positive value'
-    
-    if n < 0:
-        return fibonacci_err
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 1
+    if n <= 1:
+        return n
+
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 
