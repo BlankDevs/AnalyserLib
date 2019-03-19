7dd065088cfef72bb789cd63985a8a9e546129b5
Wisani Baloyi <wisanibaloyi65@gmail.com>
2019-03-19 13:48:53 +0200

    Python recusrion and sorting library commit

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+from recursion import sum_array, factorial, reverse, fibonacci
+from sorting import bubble_sort, merge_sort, quick_sort
+
+assert sum_array([1,2,3]) == 6
+assert factorial(5) == 120
+assert reverse('hello world') == 'dlrow olleh'
+assert fibonacci(3) = 1
+assert bubble_sort([1,3,5,2,4]) == [1, 2, 3, 4, 5]
+assert merge_sort([3,0,6,33,9]) == [0, 3, 6, 9, 33]
+assert quick_sort([3,1,7,54,2]) == [1, 2, 3, 7, 54]
