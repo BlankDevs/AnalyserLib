a98a51a60ed4d3783e2cc82a62486b5c5d8cfb0f
Blank Devs <40212192+BlankDevs@users.noreply.github.com>
2020-09-08 03:28:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='analyserlib',
-    version='1.0',
+    version='1.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='Sorting and recursion python package',
+    description='blank devs data science python functions package',
     long_description=open('README.md').read(),
-    install_requires=['numpy'],
+    install_requires=['numpy', 'pandas', 'json', 'srsly'],
     url='https://github.com/BlankDevs/analyserlib',
     author='Wisani Baloyi',
     author_email='wisanibaloyi65@gmail.com'
