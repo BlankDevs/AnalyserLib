86f18336b15e050435bfb6729d4dbdecb5a8abbd
Blank Devs <40212192+BlankDevs@users.noreply.github.com>
2020-09-08 04:12:31 +0200

    Update json_io.py

--- analyserlib/json_io.py
+++ analyserlib/json_io.py
@@ -1,4 +1,15 @@
 def read_jsonl_file(path):
+    
+    '''
+    Reads a jsonl file
+    
+    Args:
+        path (string): path to the file
+    Returns:
+        jsonl file
+ 
+    '''
+    
     labeled = []
 
     with open(path, "r") as read_file:
